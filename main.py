a4e1b9942aa9603d41964b68c63ef7a6aa287179
九歌 <67134162+DtestBug@users.noreply.github.com>
2022-03-04 13:42:30 +0800

    jenkins

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from 回文字符串 import *
 b = 10
 
 a = print(111 + b), print(222)
@@ -5,3 +6,6 @@ a = print(111 + b), print(222)
 a1 = "hello_world_yoyo"
 
 print([i for i in a1.split('_')])
+
+fun(strg)
+fun(strg1)
